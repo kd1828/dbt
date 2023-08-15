f1e6c7ef884ce484c8587d780b02c163145ccac7
Kevin Dang <kd1828@gmail.com>
2023-08-15 13:51:26 +0000

    added

--- models/marts/union_tables_by_orders_prefix.sql
+++ models/marts/union_tables_by_orders_prefix.sql
@@ -1,9 +1,13 @@
 {{ union_tables_by_prefix(
 
-      database='raw',
-      schema='dbt_learn_jinja', 
-      prefix='orders__'
+      database='dbt-tutorial-395913',
+      schema='jaffle_shop', 
+      prefix='all_days'
         
       )
       
 }}
+
+-- dbt-tutorial-395913.jaffle_shop.all_days
+
+-- select * from `dbt-tutorial-395913`.`jaffle_shop`.`raw_customers`
