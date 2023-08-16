9711498ab8e4515f120f71e35d180a9e0bc66ffc
Kevin Dang <kd1828@gmail.com>
2023-08-16 01:27:22 +0000

    amend

--- macros/grant_select.sql
+++ macros/grant_select.sql
@@ -6,7 +6,7 @@
   grant select on all views in schema {{ schema }} to role {{ role }};
   {% endset %}
 
-  {{ log('Granting select on all tables and views in schema ' ~ target.schema ~ ' to role ' ~ role, info=True) }}
+  {{ log('Granting select on all tables and views in schema ' ~ schema ~ ' to role ' ~ role, info=True) }}
   {% do run_query(sql) %}
   {{ log('Privileges granted', info=True) }}
 
