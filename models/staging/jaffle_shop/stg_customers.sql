3c204bf53900b7d290d598e19f2cb6eb7402cc6a
Kevin Dang <kd1828@gmail.com>
2023-08-18 08:22:27 +0000

    updated with source()

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -3,8 +3,13 @@ with source as (
     {#-
     Normally we would select from the table here, but we are using seeds to load
     our data in this project
-    #}
+    ----- source() -------
+    select * from {{ source('jaffle_shop', 'raw_customers') }}
+    ------- ref() -----
     select * from {{ ref('raw_customers') }}
+    #}
+
+    select * from {{ source('jaffle_shop', 'raw_customers') }}
 
 ),
 
