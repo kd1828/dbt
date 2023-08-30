b1f1fb04bf105033bdb3d1c527470af4052e9ef4
Kevin Dang <kd1828@gmail.com>
2023-08-30 13:24:29 +0000

    add

--- models/marts/stock_trades_capital.sql
+++ models/marts/stock_trades_capital.sql
@@ -1,22 +1,23 @@
-with buys as (
-    select name,
-    SUM(price) AS total_buys
-    from {{ source('jaffle_shop', 'stock_trades') }}
-    where operation = 'Buy'
-    group by name
-),
+with
+    buys as (
+        select name, sum(price) as total_buys
+        from {{ source("jaffle_shop", "stock_trades") }}
+        where operation = 'Buy'
+        group by name
+    ),
 
-sells as (
-    select name,
-    SUM(price) AS total_sells
-    from {{ source('jaffle_shop', 'stock_trades') }}
-    where operation = 'Sell'
-    group by name
-)
+    sells as (
+        select name, sum(price) as total_sells
+        from {{ source("jaffle_shop", "stock_trades") }}
+        where operation = 'Sell'
+        group by name
+    )
 
-select S.name,
-    (S.total_sells - B.total_buys) AS capital_gain_loss,
-    B.total_buys, S.total_sells
-from buys B
-join sells S on B.name = S.name
+select
+    s.name,
+    (s.total_sells - b.total_buys) as capital_gain_loss,
+    b.total_buys,
+    s.total_sells
+from buys b
+join sells s on b.name = s.name
 order by s.total_sells desc
