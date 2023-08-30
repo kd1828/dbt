a1640f8c52c86f5906f76b66b31ccb793d0aef24
Kevin Dang <kd1828@gmail.com>
2023-08-30 12:48:24 +0000

    added new re stock trades

---
+++ models/marts/stock_trades_capital.sql
@@ -0,0 +1,22 @@
+with buys as (
+    select name,
+    SUM(price) AS total_buys
+    from {{ source('jaffle_shop', 'stock_trades') }}
+    where operation = 'Buy'
+    group by name
+),
+
+sells as (
+    select name,
+    SUM(price) AS total_sells
+    from {{ source('jaffle_shop', 'stock_trades') }}
+    where operation = 'Sell'
+    group by name
+)
+
+select S.name, 
+    (S.total_sells - B.total_buys) AS capital_gain_loss,
+    B.total_buys, S.total_sells
+from buys B
+join sells S on B.name = S.name
+order by s.total_sells desc
